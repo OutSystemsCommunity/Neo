c83edddb5824b26921a0018dbd180bc771dbe5d3
David Strube <dstrube@gmail.com>
2018-12-18 12:21:22 -0500

    Adding some example Xamarin projects

---
+++ XamMasterDetail/XamMasterDetail/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamMasterDetail.Models;
+
+namespace XamMasterDetail.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ XamarinTabs/XamarinTabs/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinTabs.Models;
+
+namespace XamarinTabs.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
